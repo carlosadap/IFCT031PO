64aaf9744fdac465ca941f3438e8207508278f2c
carlosadap <38674510+Floydfish@users.noreply.github.com>
2020-08-21 16:26:38 +0200

    Add table and functions casoPtractico3

---
+++ casoPractico3/showVehicule.js
@@ -0,0 +1,11 @@
+function showVehicule() {
+  let newPar = document.createElement("p");
+  let content = document.querySelector('input[name="tipo"]:checked').value;
+  let newContent = document.createTextNode(content);
+  newPar.appendChild(newContent);
+  let currentPar = document.querySelector("#display");
+  document.body.insertBefore(newPar, currentPar)
+
+  // let where = document.querySelector("#display");
+  // where.insertAdjacentHTML('afterend', content); 
+}
