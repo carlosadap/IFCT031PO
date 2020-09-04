eb7b0e074c5384be3fefe9e5c70706aa0206ac30
Carlos Pinheiro <carlosadap@gmail.com>
2020-09-04 18:19:32 +0200

    Add handleForm and removed alert

--- foroEvaluable5/script.js
+++ foroEvaluable5/script.js
@@ -3,8 +3,16 @@ const apellidos = document.querySelector("#apellidos").value;
 const nif = document.querySelector("#nif").value;
 const email = document.querySelector("#email").value;
 const content = document.querySelector("#content");
+const form = document.querySelector("form")
 
 function showOnScreen() {
   content.textContent = `Su nombre es ${name}, sus apellidos son ${apellidos}, su NIF es ${nif} y su email es ${email}`
   alert(`Su nombre es ${name}, sus apellidos son ${apellidos}, su NIF es ${nif} y su email es ${email}`)
 }
+
+function handleForm(event) { 
+  event.preventDefault(); 
+  showOnScreen();
+} 
+
+form.addEventListener('submit', handleForm);
